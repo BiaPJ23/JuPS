3afe88f8f7163e71f2ea692619f6059cbfd2c3e8
leticia-ito <107775706+leticia-ito@users.noreply.github.com>
2024-11-29 00:08:31 -0300

    tabela da dinamica feita, porém sem armazenamento

--- dinamicas/apps.py
+++ dinamicas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccountsConfig(AppConfig):
+class DinamicasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    name = 'dinamicas'
